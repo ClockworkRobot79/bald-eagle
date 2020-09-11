19dcf76f2683ba17cb3e5b9cd56b2cde2d925a00
Eric Arnold <ClockworkRobot79@users.noreply.github.com>
2020-09-10 23:40:59 -0500

    - fix facts - add sources

--- routes/index.js
+++ routes/index.js
@@ -26,27 +26,27 @@ router.get('/bald-eagle', (req, res) => {
             img: "nest",
             text: [
                 "The largest bald eagle nest is 15 ft wide and 40 ft high.",
-                "The largest bald eagle nest is 8.2 ft wide and 13 ft high.",
+                "The largest bald eagle nest is 9.5 ft wide and 20 ft high.",
             ],
         },
         {
             img: "nest2",
             text: [
-                "A bald eagle nest can weigh up to 10,000 pounds.",
-                "A bald eagle nest can weigh up to 2,000 pounds.",
+                "A bald eagle nest can weigh up to 100,000 pounds.",
+                "A bald eagle nest can weigh up to 4,000 pounds.",
             ],
         },
         {
             img: "old",
             text: [
-                "They lay live up to 8 years in the wild, 62 years in captivity.",
-                "They lay live up to 28 years in the wild, 36 years in captivity.",
+                "They can live up to 8 years in the wild, 62 years in captivity.",
+                "They can live up to 28 years in the wild, 36 years in captivity.",
             ],
         },
         {
             img: "carry",
             text: [
-                "A mature bald eagle can lift 10-15 pounds.",
+                "A mature bald eagle can lift 40-50 pounds.",
                 "A mature bald eagle can lift 3-4 pounds.",
             ],
         },
@@ -60,7 +60,7 @@ router.get('/bald-eagle', (req, res) => {
         {
             img: "hospital",
             text: [
-                "The bald eagle got its name because the featheres on its head fall out after 5 years.",
+                "The bald eagle got its name because the feathers on its head fall out after 5 years.",
                 "The bald eagle is not really bald, it just had white feathers on its head.",
             ],
         },
